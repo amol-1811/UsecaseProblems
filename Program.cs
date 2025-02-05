11c4b8e3f24cf7125177a52e1c1cb507e5db7cf6
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-05 16:48:01 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Random random = new Random();
+        int attendanceCheck = random.Next(0, 2);
+
+        if (attendanceCheck == 0)
+        {
+            Console.WriteLine("Employee is Absent.");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Present.");
+        }
+
+        Console.ReadLine();
+    }
+}
