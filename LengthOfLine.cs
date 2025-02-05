fb0707e532719e038ebee4dcbfc7c0f148a99765
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-05 20:54:22 +0530

    UseCase 1st for line problem

---
+++ LengthOfLine.cs
@@ -0,0 +1,24 @@
+﻿namespace Employee_Attandance
+{
+    public class LineLengthCalculator
+    {
+        public static void LengthOfLine()
+        {
+            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+            Console.Write("x1 = ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+            Console.Write("x2 = ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("The length of the line is: " + length);
+        }
+    }
+}
