d9ffc8cdd86c5448d59b6d8526be4883a1d6860e
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-05 17:06:36 +0530

    Use case two implemented

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 
-class Program
+public class EmployeeWage
 {
     public static void Main(string[] args)
     {
-        
+        const int WAGE_PER_HOUR = 20;
+        const int FULL_DAY_HOURS = 8;
+
         Random random = new Random();
-        int attendanceCheck = random.Next(0, 2);
+        int attendanceCheck = random.Next(0, 2); 
+
+        int dailyWage = 0;
+        if (attendanceCheck == 1) 
+        {
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+        }
 
         if (attendanceCheck == 0)
         {
-            Console.WriteLine("Employee is Absent.");
+            Console.WriteLine("Employee is Absent. Daily Wage: 0");
         }
         else
         {
-            Console.WriteLine("Employee is Present.");
+            Console.WriteLine($"Employee is Present. Daily Wage: {dailyWage}");
         }
 
         Console.ReadLine();
